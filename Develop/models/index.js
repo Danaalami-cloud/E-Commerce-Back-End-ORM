ac7c7a12249d599002d05db04abdab738d905d2c
Dana Alami <dana.alami18@gmail.com>
2021-05-27 14:19:27 +0300

    defined the belonging products and categories

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+
+}),
 
 // Categories have many Products
+Categories.haveMany(Product,{
 
+}),
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: {
+    model: ProductTag,
+    unique: false
 
+  },
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+});
 module.exports = {
   Product,
   Category,
